e4d0a7c91faf27aaab712ed29fd5fb4b5d762447
Kasey Reynolds <reynolds-8@hotmail.com>
2024-05-02 10:14:09 -0400

    completed prompt4

--- script.js
+++ script.js
@@ -86,33 +86,83 @@ const inventors = [
 
 // Hint:  Return a new object literal from the callback that looks something like { first: "First Name", last: "Last Name" }
 
-let inventorNames = []
+// let inventorNames = []
 
 
 // Enter your solution code here:
 
-let inventorsNames = inventors.map((inventor) => {
-    const newInventor = {First: `${inventor.first}`, Last: `${inventor.last}`}
-    return newInventor
-})
+// let inventorsNames = inventors.map((inventor) => {
+//     const newInventor = {First: `${inventor.first}`, Last: `${inventor.last}`}
+//     return newInventor
+// })
 
 
 // Check your return value:
-console.log('Exercise 2 My Result: ', inventorNames)
-console.log('Exercise 2 Correct Result: ',
-    [
-        { first: 'Albert', last: 'Einstein' },
-        { first: 'Isaac', last: 'Newton' },
-        { first: 'Galileo', last: 'Galilei' },
-        { first: 'Marie', last: 'Curie' },
-        { first: 'Johannes', last: 'Kepler' },
-        { first: 'Nicolaus', last: 'Copernicus' },
-        { first: 'Max', last: 'Planck' },
-        { first: 'Katherine', last: 'Blodgett' },
-        { first: 'Ada', last: 'Lovelace' },
-        { first: 'Sarah E.', last: 'Goode' },
-        { first: 'Lise', last: 'Meitner' },
-        { first: 'Hanna', last: 'Hammarström' }
-    ]
-)
+// console.log('Exercise 2 My Result: ', inventorNames)
+// console.log('Exercise 2 Correct Result: ',
+//     [
+//         { first: 'Albert', last: 'Einstein' },
+//         { first: 'Isaac', last: 'Newton' },
+//         { first: 'Galileo', last: 'Galilei' },
+//         { first: 'Marie', last: 'Curie' },
+//         { first: 'Johannes', last: 'Kepler' },
+//         { first: 'Nicolaus', last: 'Copernicus' },
+//         { first: 'Max', last: 'Planck' },
+//         { first: 'Katherine', last: 'Blodgett' },
+//         { first: 'Ada', last: 'Lovelace' },
+//         { first: 'Sarah E.', last: 'Goode' },
+//         { first: 'Lise', last: 'Meitner' },
+//         { first: 'Hanna', last: 'Hammarström' }
+//     ]
+// )
+
+// Array.prototype.sort()
+
+// 3. Sort the inventors by birth date in ascending order (from those born furthest in the past to those born most recently).
+
+//let sortedByBirthYear = []
+
+// Enter your solution code here:
+
+// let sortedByBirthYear = birthYear.sort()
+// console.log(year)
+
+// // Check your return value:
+// console.log('Exercise 3 My Result: ', sortedByBirthYear)
+// console.log('Exercise 3 Correct Result: ',
+//     [
+//         { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+//         { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+//         { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+//         { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+//         { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+//         { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 },
+//         { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+//         { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+//         { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+//         { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+//         { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+//         { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 }
+//     ]
+// )
+
+// Array.prototype.find()
+
+// 4. Use Array.prototype.find() to identify the inventor with the first name 'Ada' from an array of inventor objects
+
+// - You have an array of objects, each representing an inventor with various properties including their first name.
+// -  Utilize the Array.prototype.find() method to locate the object for the inventor named 'Ada'.
+// -  Assign the found inventor object to the variable 'inventorNamedAda'
+
+let inventorNamedAda = {}
+
+// // Enter you solution code here:
+
+inventorNamedAda = inventors.find(inventor => inventor.first == "Ada")
+
+// // Check your return value:
+console.log('Exercise 4 My Result: ', inventorNamedAda)
+console.log('Exercise 4 Correct Result: ', { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 })
+
+
 
